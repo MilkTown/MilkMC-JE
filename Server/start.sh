ac184f156c1fcf08554d5dc535ecbdc9bbb439d8
Hailaylin <hailay@qq.com>
2017-08-19 16:01:04 +0800

    v0.8
    更新资源地狱资源末地，换12G服务器 更换配置

--- Server/start.sh
+++ Server/start.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-while true;do java -server -d64 -Xmx3550M -Xms3550M -XX:+AggressiveOpts -XX:+UseCompressedOops -jar PaperSpigot-1.12-b1169.jar;done
+java -server -d64 -Xmx8000M -Xms3000M -XX:+AggressiveOpts -XX:+UseCompressedOops -jar PaperSpigot-1.12-b1169.jar
