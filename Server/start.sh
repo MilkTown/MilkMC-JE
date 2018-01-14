5c34885955f9e961e92211ef53f12c20e1c08a42
Hailaylin <hailay@qq.com>
2018-01-14 12:07:28 +0800

    以前的更改？

--- Server/start.sh
+++ Server/start.sh
@@ -1,1 +1,1 @@
-java -server -d64 -Xmx10000M -Xms4000M -XX:+AggressiveOpts -XX:+UseCompressedOops -jar PaperSpigot-1.12-b1169.jar
+java -server -d64 -Xmx1500M -Xms512M -XX:+AggressiveOpts -XX:+UseCompressedOops -jar PaperSpigot-1.12-b1169.jar
