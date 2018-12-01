c122dc1be2d913bdf16e5f0b65afa3425810fa6d
Hailay <Hailay@qq.com>
2018-12-01 22:45:21 +0800

    上传顺序修改
    文件太多传不过来

--- Server/start.bat
+++
@@ -1,1 +0,0 @@
-java -server -d64 -Xmx1024M -Xms512M -XX:+AggressiveOpts -XX:+UseCompressedOops -jar PaperSpigot-1.12-b1169.jar
--- Server/start.sh
+++ Server/start.sh
@@ -1,1 +1,20 @@
-java -server -d64 -Xmx1500M -Xms512M -XX:+AggressiveOpts -XX:+UseCompressedOops -jar PaperSpigot-1.12-b1169.jar
+#!/bin/bash
+
+server="【风车小镇】Windmill-Town"
+mc="Spigot-1.12-b1167.jar"
+SleepTime=2
+
+echo "############################"
+echo "#                          #"
+echo "#服务器正在启动，请稍后....#"
+echo "#$server #"
+echo "############################"
+
+
+function main(){
+        sleep $SleepTime
+        java -Xms512M -Xmx2G -Dfml.ignorePatchDiscrepancies=true -XX:+AggressiveOpts -XX:+UseCompressedOops -Xincgc -server -jar $mc
+}
+
+main
+
